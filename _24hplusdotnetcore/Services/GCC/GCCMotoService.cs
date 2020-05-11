4fb8a169466b35ce1c21752fdf34c8ff838989bb
Phat Nguyen <phatndt@datxanh.com.vn>
2020-05-11 11:47:59 +0700

    update server gcc

--- _24hplusdotnetcore/Services/GCC/GCCMotoService.cs
+++ _24hplusdotnetcore/Services/GCC/GCCMotoService.cs
@@ -128,7 +128,7 @@ namespace _24hplusdotnetcore.Services.GCC
                         request.AddParameter("product_code", body.product_code);
                         request.AddParameter("agency_id", body.agency_id);
                         request.AddParameter("program", body.program);
-                        request.AddParameter("package", body.package);
+                        request.AddParameter("package", package);
                         request.AddParameter("request_code", body.request_code);
                         request.AddParameter("buy_fullname", body.buy_fullname);
                         request.AddParameter("buy_bod", body.buy_bod);
