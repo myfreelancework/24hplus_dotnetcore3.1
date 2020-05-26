b1ea818793642cd98c1eca065938966a0409b3b2
Phat Nguyen <phatndt@datxanh.com.vn>
2020-05-26 07:32:08 +0700

    update customerModel

--- _24hplusdotnetcore/Models/Customer.cs
+++ _24hplusdotnetcore/Models/Customer.cs
@@ -17,26 +17,12 @@ namespace _24hplusdotnetcore.Models
         public string GreenType { get; set; } // @todo
         public DateTime CreatedDate { get; set; }
         public DateTime ModifiedDate { get; set; }
-
         public Personal Personal { get; set; } // 80%
         public Address ResidentAddress { get; set; } // @todo
         public Boolean IsTheSameResidentAddress { get; set; }
         public Address TemporaryAddress { get; set; } // @todo
-        public Living Living { get; set; }
         public Working Working { get; set; }
         public Loan Loan { get; set; }
-        public Family Family { get; set; }
-
-
-        public ReferencePerson ReferencePerson1 { get; set; }
-        public ReferencePerson ReferencePerson2 { get; set; }
-
-
-        public Vender Vender1 { get; set; }
-        public Vender Vender2 { get; set; }
-        public InsuranceContract InsuranceContract { get; set; }
-        public CreditContract CreditContract { get; set; }
-        public Disburement Disburement { get; set; }
         public Sale SaleInfo { get; set; }
         public Result Result { get; set; }
     }
@@ -62,20 +48,10 @@ namespace _24hplusdotnetcore.Models
         public string Ward { get; set; }
         public string Street { get; set; }
     }
-
-    public class Living
-    {
-        public string StayedYear { get; set; }
-        public string StayedMonth { get; set; }
-        public string HouseType { get; set; }
-    }
-
     public class Working
     {
         public string Job { get; set; }
         public string Position { get; set; }
-        public string Year { get; set; }
-        public string Month { get; set; }
         public string TaxId { get; set; }
         public string Income { get; set; }
         public string IncomeMethod { get; set; }
@@ -85,10 +61,6 @@ namespace _24hplusdotnetcore.Models
         public string CompanyName { get; set; }
         public string CompanyPhone { get; set; }
         public Address CompanyAddress { get; set; }
-
-        public string BranchName { get; set; }
-        public string BranchPhone { get; set; }
-        public Address BranchAddress { get; set; }
     }
 
     public class Sale
@@ -107,64 +79,14 @@ namespace _24hplusdotnetcore.Models
         public string Name { get; set; }
         public string Amount { get; set; }
         public string BuyInsurance { get; set; }
-    }
-
-    public class Disburement
-    {
         public string SignAddress { get; set; }
-        public string Method { get; set; }
-        public string Name { get; set; }
-        public string BankName { get; set; }
-        public string BankBranch { get; set; }
-        public string BankAccount { get; set; }
     }
 
-    public class Vender
-    {
-        public string Name { get; set; }
-        public string Phone { get; set; }
-    }
-
-    public class ReferencePerson
-    {
-        public string Name { get; set; }
-        public string Phone { get; set; }
-        public string Relationship { get; set; }
-    }
-
-    public class Family
-    {
-        public string Relationship { get; set; }
-        public string OtherRelationship { get; set; }
-        public string Name { get; set; }
-        public string DateOfBirth { get; set; }
-        public string IdCard { get; set; }
-        public string Phone { get; set; }
-        public string Dependency { get; set; }
-        public string Address { get; set; }
-    }
     public class Result
     {
         public string Department { get; set; }
         public string Status { get; set; }
         public string Reason { get; set; }
         public string Note { get; set; }
-    }
-    public class InsuranceContract
-    {
-        public string CompanyName { get; set; }
-        public string MonthlyFee { get; set; }
-        public string Period { get; set; }
-        public string OtherPeriod { get; set; }
-        public string AvgBill { get; set; }
-        public string AvgBalance { get; set; }
-        public string VehiclePrice { get; set; }
-    }
-    public class CreditContract
-    {
-        public string ContractNumber { get; set; }
-        public string StartDate { get; set; }
-        public string Period { get; set; }
-        public string MonthlyFee { get; set; }
     }
 }
