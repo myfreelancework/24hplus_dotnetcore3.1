d69212a3e15a08a8a61c90e87d25495df65c7d8d
Chuong Nguyen <cnguyen67@dxc.com>
2020-06-01 22:09:04 +0700

    update temp func

--- _24hplusdotnetcore/Common/Constants/Url.cs
+++ _24hplusdotnetcore/Common/Constants/Url.cs
@@ -18,5 +18,6 @@ namespace _24hplusdotnetcore.Common.Constants
         //CRM
         public static string CRMLogin = "http://ebanker-vn.com/24hplus/vtigercrm/modules/Mobile/api.php";
         public static string CRMQueryLead = "http://ebanker-vn.com/24hplus/vtigercrm/modules/Mobile/api.php";
+        public static string CRMURL = "http://24hplus.ebanker-vn.com/uat_s1/modules/Mobile/api.php";
     }
 }
---
+++ _24hplusdotnetcore/Services/CRM/DataCRMProcessingServices.cs
@@ -0,0 +1,41 @@
+﻿using _24hplusdotnetcore.Models;
+using _24hplusdotnetcore.Models.CRM;
+using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
+using System;
+
+namespace _24hplusdotnetcore.Services.CRM
+{
+    public class DataCRMProcessingServices
+    {
+        private readonly ILogger<DataCRMProcessingServices> _logger;
+        private readonly IMongoCollection<DataCRMProcessing> _dataCRMProcessing;
+        public DataCRMProcessingServices(ILogger<DataCRMProcessingServices> logger, IMongoDbConnection connection)
+        {
+            _logger = logger;
+            var client = new MongoClient(connection.ConnectionString);
+            var database = client.GetDatabase(connection.DataBase);
+            _dataCRMProcessing = database.GetCollection<DataCRMProcessing>(Common.MongoCollection.DataCRMProcessing);
+        }
+
+        public DataCRMProcessing CreateOne(DataCRMProcessing dataCRM)
+        {
+            var newData = new DataCRMProcessing();
+            try
+            {
+                _dataCRMProcessing.InsertOne(dataCRM);
+                newData = dataCRM;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+            }
+            return newData;
+        }
+
+        public long UpdateByCustomerId(string CustomerId, string Status)
+        {
+            return 1;
+        }
+    }
+}
