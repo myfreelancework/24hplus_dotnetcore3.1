44d3dc984faab1693c4f1478869344e3c8538127
phat nguyen <phatndt@datxanh.com.vn>
2020-06-25 10:07:27 +0700

    update file upload model

--- _24hplusdotnetcore/Models/FileUpload.cs
+++ _24hplusdotnetcore/Models/FileUpload.cs
@@ -1,4 +1,5 @@
 ﻿using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
 
 namespace _24hplusdotnetcore.Models
 {
@@ -6,10 +7,25 @@ namespace _24hplusdotnetcore.Models
     {
         [BsonId]
         [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        [JsonProperty("fileUploadId")]
         public string FileUploadId { get; set; }
+        [JsonProperty("documentCategoryId")]
         public string DocumentCategoryId { get; set; }
+        [JsonProperty("fileUploadName")]
         public string FileUploadName { get; set; }
+        [JsonProperty("fileUploadURL")]
         public string FileUploadURL { get; set; }
+        [JsonProperty("customerId")]
         public string CustomerId { get; set; }
+        [JsonProperty("groupId")]
+        public string GroupId { get; set; }
+        [JsonProperty("documentCode")]
+        public string DocumentCode { get; set; }
+        [JsonProperty("documentName")]
+        public string DocumentName { get; set; }
+        [JsonProperty("inputDocUid")]
+        public string InputDocUid { get; set; }
+        [JsonProperty("mapBpmVar")]
+        public string MapBpmVar { get; set; }
     }
 }
