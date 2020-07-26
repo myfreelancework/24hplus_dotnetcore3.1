6b8b79dac0f7544dc14cc616ba2131ce90e78077
Phat Nguyen <phatndt@datxanh.com.vn>
2020-07-27 00:58:18 +0700

    update cusomer info to crm: resident address

--- _24hplusdotnetcore/Common/Config.cs
+++ _24hplusdotnetcore/Common/Config.cs
@@ -3,5 +3,8 @@ namespace _24hplusdotnetcore.Common
     public static class Config
     {
         public const int PageSize = 20;
+        public const string PROVINCES = "Provinces";
+        public const string DISTRICTS = "Districts";
+        public const string WARDS = "Wards";
     }
 }
